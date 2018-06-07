52dc086db9a6c125601ca9c5ec8cdcff3af7b0bd
Ezinwa Okpoechi <brainmaestro@outlook.com>
2018-06-07 10:41:54 +0200

    Format code with rustfmt

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,6 +1,6 @@
 // This is an adaption of the PrettyFormatter from serde_json
+use serde_json;
 use std::io;
-use ::serde_json;
 
 #[derive(Clone, Debug)]
 pub struct PrettyFormatter<'a> {
@@ -13,17 +13,13 @@ pub struct PrettyFormatter<'a> {
 
 impl<'a> PrettyFormatter<'a> {
     /// Construct a pretty printer formatter that uses the `indent` string for indentation.
-    pub fn new(indent: &'a [u8],
-               before_colon: &'a [u8],
-               after_colon: &'a [u8]
-    ) -> Self
-    {
+    pub fn new(indent: &'a [u8], before_colon: &'a [u8], after_colon: &'a [u8]) -> Self {
         PrettyFormatter {
             current_indent: 0,
             has_value: false,
             indent,
             before_colon,
-            after_colon
+            after_colon,
         }
     }
 }
@@ -31,8 +27,8 @@ impl<'a> PrettyFormatter<'a> {
 impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
     #[inline]
     fn begin_array<W: ?Sized>(&mut self, writer: &mut W) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         self.current_indent += 1;
         self.has_value = false;
@@ -41,8 +37,8 @@ impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
 
     #[inline]
     fn end_array<W: ?Sized>(&mut self, writer: &mut W) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         self.current_indent -= 1;
 
@@ -56,8 +52,8 @@ impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
 
     #[inline]
     fn begin_array_value<W: ?Sized>(&mut self, writer: &mut W, first: bool) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         if first {
             try!(writer.write_all(b"\n"));
@@ -70,8 +66,8 @@ impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
 
     #[inline]
     fn end_array_value<W: ?Sized>(&mut self, _writer: &mut W) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         self.has_value = true;
         Ok(())
@@ -79,8 +75,8 @@ impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
 
     #[inline]
     fn begin_object<W: ?Sized>(&mut self, writer: &mut W) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         self.current_indent += 1;
         self.has_value = false;
@@ -89,8 +85,8 @@ impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
 
     #[inline]
     fn end_object<W: ?Sized>(&mut self, writer: &mut W) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         self.current_indent -= 1;
 
@@ -104,8 +100,8 @@ impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
 
     #[inline]
     fn begin_object_key<W: ?Sized>(&mut self, writer: &mut W, first: bool) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         if first {
             try!(writer.write_all(b"\n"));
@@ -117,19 +113,18 @@ impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
 
     #[inline]
     fn begin_object_value<W: ?Sized>(&mut self, writer: &mut W) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         writer.write_all(self.before_colon)?;
         writer.write_all(b":")?;
         writer.write_all(self.after_colon)
-
     }
 
     #[inline]
     fn end_object_value<W: ?Sized>(&mut self, _writer: &mut W) -> io::Result<()>
-        where
-            W: io::Write,
+    where
+        W: io::Write,
     {
         self.has_value = true;
         Ok(())
@@ -137,8 +132,8 @@ impl<'a> serde_json::ser::Formatter for PrettyFormatter<'a> {
 }
 
 fn indent<W: ?Sized>(wr: &mut W, n: usize, s: &[u8]) -> io::Result<()>
-    where
-        W: io::Write,
+where
+    W: io::Write,
 {
     for _ in 0..n {
         try!(wr.write_all(s));
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 #![recursion_limit = "1024"]
 
-#[macro_use] extern crate error_chain;
-extern crate serde_json;
-extern crate serde_transcode;
+#[macro_use]
+extern crate error_chain;
 extern crate clap;
 extern crate filebuffer;
+extern crate serde_json;
+extern crate serde_transcode;
 
-use std::io::{self, BufReader, BufWriter, Read};
-use std::fs::{self, File};
 use std::ffi::{OsStr, OsString};
+use std::fs::{self, File};
+use std::io::{self, BufReader, BufWriter, Read};
+
 use clap::{App, Arg};
 use filebuffer::FileBuffer;
 
@@ -27,13 +29,13 @@ mod errors {
 }
 use errors::*;
 
-fn is_file(path: &OsStr) -> std::result::Result<(), OsString>
-{
-    File::open(&path).map_err(|_| OsString::from("file not found")).and(Ok(()))
+fn is_file(path: &OsStr) -> std::result::Result<(), OsString> {
+    File::open(&path)
+        .map_err(|_| OsString::from("file not found"))
+        .and(Ok(()))
 }
 
-fn prettify<S: Read, F: serde_json::ser::Formatter>(source: S, formatter: F) -> Result<()>
-{
+fn prettify<S: Read, F: serde_json::ser::Formatter>(source: S, formatter: F) -> Result<()> {
     let writer = BufWriter::new(io::stdout());
 
     let mut deserializer = serde_json::Deserializer::from_reader(source);
@@ -42,28 +44,35 @@ fn prettify<S: Read, F: serde_json::ser::Formatter>(source: S, formatter: F) -> 
     Ok(())
 }
 
-fn main() -> Result<()>
-{
+fn main() -> Result<()> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
-    .author(env!("CARGO_PKG_AUTHORS"))
-    .version(env!("CARGO_PKG_VERSION"))
-    .about(env!("CARGO_PKG_DESCRIPTION"))
-    .arg(Arg::with_name("input")
-        .help("the input.json file to use")
-        .validator_os(is_file))
-    .arg(Arg::with_name("ident")
-        .help("this string is used as one-level ident")
-        .short("i")
-        .default_value("  "))
-    .arg(Arg::with_name("before-colon")
-        .help("this string is inserted before :")
-        .short("b")
-        .default_value(""))
-    .arg(Arg::with_name("after-colon")
-        .help("this string is inserted after :")
-        .short("a")
-        .default_value(" "))
-    .get_matches();
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::with_name("input")
+                .help("the input.json file to use")
+                .validator_os(is_file),
+        )
+        .arg(
+            Arg::with_name("ident")
+                .help("this string is used as one-level ident")
+                .short("i")
+                .default_value("  "),
+        )
+        .arg(
+            Arg::with_name("before-colon")
+                .help("this string is inserted before :")
+                .short("b")
+                .default_value(""),
+        )
+        .arg(
+            Arg::with_name("after-colon")
+                .help("this string is inserted after :")
+                .short("a")
+                .default_value(" "),
+        )
+        .get_matches();
 
     let formatter = formatter::PrettyFormatter::new(
         matches.value_of("ident").unwrap().as_bytes(),
