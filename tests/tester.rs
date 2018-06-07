a7b34b8b51574b0921247533f8e6e293e662e6b2
Ezinwa Okpoechi <brainmaestro@outlook.com>
2018-06-07 11:42:46 +0200

    Add basic tests

---
+++ tests/tester.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::fs::read_to_string;
+use std::path::PathBuf;
+use std::process::Command;
+
+pub struct Tester {
+    exe: PathBuf,
+}
+
+impl Tester {
+    pub fn new() -> Self {
+        let root = env::current_exe()
+            .expect("tests executable")
+            .parent()
+            .expect("tests executable directory")
+            .parent()
+            .expect("pp executable directory")
+            .to_path_buf();
+
+        let exe_name = if cfg!(windows) { "pp.exe" } else { "pp" };
+
+        Tester {
+            exe: root.join(exe_name),
+        }
+    }
+
+    pub fn test(&self, input: &str, prettified: &str) {
+        let input = format!("tests/json/{}", input);
+        let prettified = format!("tests/json/{}", prettified);
+
+        let output = Command::new(&self.exe)
+            .arg(input)
+            .output()
+            .expect("pp failed")
+            .stdout;
+
+        let actual = String::from_utf8_lossy(&output);
+        let expected = read_to_string(prettified).expect("could not read prettified file");
+
+        assert_eq!(expected, actual);
+    }
+}
